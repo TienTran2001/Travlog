c70147534265c5594dc1fcf1b508fea5c067af36
Tiến Trần <tienco201@gmail.com>
2024-07-05 15:26:51 +0700

    add scroll count

--- src/pages/Home/TravelPoint.tsx
+++ src/pages/Home/TravelPoint.tsx
@@ -2,6 +2,8 @@ import discount from "../../assets/images/travelPoint/discount.svg";
 import CountUp from "react-countup";
 import ticketStar from "../../assets/images/icons/ticket-star.svg";
 import Ticket from "../../components/Ticket/Ticket";
+import ScrollTrigger from "react-scroll-trigger";
+import { useState } from "react";
 
 const travelPoints = [
   {
@@ -27,47 +29,59 @@ const travelPoints = [
 ];
 
 const TravelPoint = () => {
+  const [counter, setCounter] = useState(false);
   return (
-    <div className="">
-      <div className="container mt-[128px]">
-        <div className="flex flex-col lg:flex-row">
-          <div className="bg-travel-point lg:w-1/2">
-            <Ticket className="lg:absolute lg:right-0 lg:top-1/4">
-              <img src={discount} alt="discount" className="bound-animation" />
-              Discounted Price
-            </Ticket>
-          </div>
-          <div className="relative mt-[50px] text-center lg:mt-0 lg:w-1/2 lg:pl-[90px] lg:text-start">
-            <div className="absolute right-0 top-1/2 hidden -translate-y-1/2 translate-x-1/2 rounded-full bg-yellow-custom px-[36px] py-[38px] lg:block">
-              <img src={ticketStar} alt="ticket start" />
+    <ScrollTrigger
+      onEnter={() => setCounter(true)}
+      onExit={() => setCounter(false)}
+    >
+      <div className="">
+        <div className="container mt-[128px]">
+          <div className="flex flex-col lg:flex-row">
+            <div className="bg-travel-point lg:w-1/2">
+              <Ticket className="lg:absolute lg:right-0 lg:top-1/4">
+                <img
+                  src={discount}
+                  alt="discount"
+                  className="bound-animation"
+                />
+                Discounted Price
+              </Ticket>
             </div>
-            <h3 className="title-heading">Travel Point</h3>
-            <h2 className="title-heading-2">
-              We helping you find your dream location
-            </h2>
-            <p className="mt-8 text-desc">
-              Contrary to popular belief, Lorem Ipsum is not simply random text.
-              It has roots in a piece of classical Latin literature from 45 BC.
-            </p>
-            <div className="mt-[64px] grid grid-cols-12 gap-8 lg:px-0">
-              {travelPoints.length > 0 &&
-                travelPoints.map((item, index) => (
-                  <div
-                    key={index}
-                    className="col-span-12 rounded-[32px] border border-gray-light p-8 text-center md:col-span-6"
-                  >
-                    <span className="text-[35px] font-bold text-orange-custom">
-                      <CountUp end={item.numbers} />
-                      <span>{item.characters}</span>
-                    </span>
-                    <p className="mt-4 text-lg">{item.name}</p>
-                  </div>
-                ))}
+            <div className="relative mt-[50px] text-center lg:mt-0 lg:w-1/2 lg:pl-[90px] lg:text-start">
+              <div className="absolute right-0 top-1/2 hidden -translate-y-1/2 translate-x-1/2 rounded-full bg-yellow-custom px-[36px] py-[38px] lg:block">
+                <img src={ticketStar} alt="ticket start" />
+              </div>
+              <h3 className="title-heading">Travel Point</h3>
+              <h2 className="title-heading-2">
+                We helping you find your dream location
+              </h2>
+              <p className="mt-8 text-desc">
+                Contrary to popular belief, Lorem Ipsum is not simply random
+                text. It has roots in a piece of classical Latin literature from
+                45 BC.
+              </p>
+              <div className="mt-[64px] grid grid-cols-12 gap-8 lg:px-0">
+                {travelPoints.length > 0 &&
+                  travelPoints.map((item, index) => (
+                    <div
+                      key={index}
+                      className="col-span-12 rounded-[32px] border border-gray-light p-8 text-center md:col-span-6"
+                    >
+                      <span className="text-[35px] font-bold text-orange-custom">
+                        {counter && <CountUp end={item.numbers} />}
+
+                        <span>{item.characters}</span>
+                      </span>
+                      <p className="mt-4 text-lg">{item.name}</p>
+                    </div>
+                  ))}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </ScrollTrigger>
   );
 };
 
