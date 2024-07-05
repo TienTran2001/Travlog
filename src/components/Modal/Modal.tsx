ef2c4c4901ad33337bd212e4b0d28ae29e70f51c
Tiến Trần <tienco201@gmail.com>
2024-07-05 09:53:49 +0700

    add  video

---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+interface IProps {
+  children: React.ReactNode;
+  open: boolean;
+  handleClose: () => void;
+}
+
+// createPortal
+const Modal = ({ children, open = false, handleClose = () => {} }: IProps) => {
+  if (typeof document === "undefined") return <div className="modal"></div>;
+
+  return ReactDOM.createPortal(
+    <div
+      className={`modal fixed inset-0 z-50 flex items-center justify-center p-5 transition-all ${open ? "" : "invisible opacity-0"}`}
+    >
+      <div
+        className="absolute inset-0 bg-black opacity-25 overlay"
+        onClick={handleClose}
+      ></div>
+      <div className="modal-content relative z-10 h-full max-h-[900px] w-full max-w-container rounded-lg bg-white bg-opacity-20 p-2">
+        {children}
+      </div>
+    </div>,
+    document.querySelector("body")!,
+  );
+};
+
+export default Modal;
--- src/pages/Home/Explore.tsx
+++ src/pages/Home/Explore.tsx
@@ -7,75 +7,98 @@ import explore3 from "../../assets/images/explore/Rectangle3.png";
 import send from "../../assets/images/icons/send1.svg";
 import addUser from "../../assets/images/icons/add-user.svg";
 import { HiLocationMarker } from "react-icons/hi";
+import { useState } from "react";
+import Modal from "../../components/Modal/Modal";
 
 const Explore = () => {
+  const [open, setOpen] = useState(false);
   return (
-    <div className="container mt-[100px] flex flex-col items-center overflow-hidden px-[15px] md:mt-16 md:flex-row md:overflow-visible">
-      {/* div left */}
-      <div className="flex flex-col gap-y-[43px] md:w-1/2 md:pr-[90px]">
-        <div className="flex items-center justify-center gap-x-4 rounded-full bg-white px-8 py-4 shadow-primary md:self-start">
-          <span className="font-bold text-pink-light">Explore the world!</span>
-          <img src={bagIcon} alt="" className="bound-animation" />
+    <>
+      {open && (
+        <Modal open={open} handleClose={() => setOpen(false)}>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/wO5hfqSUocE?si=JGIhE5n0qNERcUVD"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
+          ></iframe>
+        </Modal>
+      )}
+
+      <div className="container mt-[100px] flex flex-col items-center overflow-hidden px-[15px] md:mt-16 md:flex-row md:overflow-visible">
+        {/* div left */}
+        <div className="flex flex-col gap-y-[43px] md:w-1/2 md:pr-[90px]">
+          <div className="flex items-center justify-center px-8 py-4 bg-white rounded-full gap-x-4 shadow-primary md:self-start">
+            <span className="font-bold text-pink-light">
+              Explore the world!
+            </span>
+            <img src={bagIcon} alt="" className="bound-animation" />
+          </div>
+          <h2 className="text-center text-4xl font-bold md:text-start md:text-[69px] md:leading-[100px]">
+            Travel <span className="text-pink-light">top destination</span> of
+            the world
+          </h2>
+          <p className="text-desc">
+            We always make our customer happy by providing as many choices as
+            possible
+          </p>
+          <div className="flex flex-col gap-4 md:flex-row">
+            <Button variant="PRIMARY" className="shadow-primary">
+              Get Started
+            </Button>
+            <Button
+              variant="OUTLINED"
+              className="flex items-center justify-center gap-x-4"
+              onClick={() => setOpen(true)}
+            >
+              <div className="relative">
+                <div className="absolute right-[1px] top-[1px] h-[18px] w-[18px] animate-ping rounded-full border-[4px] border-main"></div>
+                <img src={playIcon} alt="" className="" />
+              </div>
+              <span>Watch Demo</span>
+            </Button>
+          </div>
         </div>
-        <h2 className="text-center text-4xl font-bold md:text-start md:text-[69px] md:leading-[100px]">
-          Travel <span className="text-pink-light">top destination</span> of the
-          world
-        </h2>
-        <p className="text-desc">
-          We always make our customer happy by providing as many choices as
-          possible
-        </p>
-        <div className="flex flex-col gap-4 md:flex-row">
-          <Button variant="PRIMARY" className="shadow-primary">
-            Get Started
-          </Button>
-          <Button
-            variant="OUTLINED"
-            className="flex items-center justify-center gap-x-4"
-          >
+        {/* div right */}
+        <div className="bg-custom-explore mt-[50px] flex w-full flex-col items-center gap-[32px] pt-[83px] md:mt-0 md:w-1/2 md:flex-row">
+          <div className="">
             <div className="relative">
-              <div className="absolute right-[1px] top-[1px] h-[18px] w-[18px] animate-ping rounded-full border-[4px] border-main"></div>
-              <img src={playIcon} alt="" className="" />
+              <img
+                src={explore1}
+                alt="explore1"
+                className="jump-animation rounded-[32px]"
+              />
+              <div className="jump-animation-1 absolute -bottom-[10%] -left-[13%] rounded-full bg-pink-light p-4 shadow-lg">
+                <img src={send} alt="send" className="" />
+              </div>
             </div>
-            <span>Watch Demo</span>
-          </Button>
-        </div>
-      </div>
-      {/* div right */}
-      <div className="bg-custom-explore mt-[50px] flex w-full flex-col items-center gap-[32px] pt-[83px] md:mt-0 md:w-1/2 md:flex-row">
-        <div className="">
+            <img
+              src={explore2}
+              alt="explore2"
+              className="jump-animation mt-[32px] rounded-[32px]"
+            />
+          </div>
           <div className="relative">
             <img
-              src={explore1}
-              alt="explore1"
-              className="jump-animation rounded-[32px]"
+              src={explore3}
+              alt="explore3"
+              className="md:full jump-animation-2 h-[300px] w-[272px] rounded-[32px] object-cover md:h-full md:w-full"
             />
-            <div className="jump-animation-1 absolute -bottom-[10%] -left-[13%] rounded-full bg-pink-light p-4 shadow-lg">
-              <img src={send} alt="send" className="" />
+            <div className="jump-animation absolute -left-[13%] bottom-full rounded-full bg-orange-custom p-4 shadow-lg md:-bottom-[30%] md:left-[30%]">
+              <img className="" src={addUser} alt="add-user" />
             </div>
-          </div>
-          <img
-            src={explore2}
-            alt="explore2"
-            className="jump-animation mt-[32px] rounded-[32px]"
-          />
-        </div>
-        <div className="relative">
-          <img
-            src={explore3}
-            alt="explore3"
-            className="md:full jump-animation-2 h-[300px] w-[272px] rounded-[32px] object-cover md:h-full md:w-full"
-          />
-          <div className="jump-animation absolute -left-[13%] bottom-full rounded-full bg-orange-custom p-4 shadow-lg md:-bottom-[30%] md:left-[30%]">
-            <img className="" src={addUser} alt="add-user" />
-          </div>
-          <div className="jump-animation absolute -right-[10%] top-2/3 flex items-center gap-x-2 rounded-full bg-white px-8 py-4 shadow-primary md:-right-[40%]">
-            <HiLocationMarker color="#FACD49" size={30} />
-            <span className="text-sm font-bold">Top Places</span>
+            <div className="jump-animation absolute -right-[10%] top-2/3 flex items-center gap-x-2 rounded-full bg-white px-8 py-4 shadow-primary md:-right-[40%]">
+              <HiLocationMarker color="#FACD49" size={30} />
+              <span className="text-sm font-bold">Top Places</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
