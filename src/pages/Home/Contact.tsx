8d5a05f794487f40a25bb24114fbeb8f23340316
Tiến Trần <tienco201@gmail.com>
2024-07-10 09:40:02 +0700

    fixed responsive ui

--- src/pages/Home/Contact.tsx
+++ src/pages/Home/Contact.tsx
@@ -15,8 +15,8 @@ const Contact = () => {
 
   return (
     <div className="container mt-[128px]">
-      <div className="relative rounded-[32px] bg-yellow-light px-[15px] py-[128px] text-center lg:px-16">
-        <div className="absolute top-0 left-0 -translate-x-4 -translate-y-4 lg:-translate-x-1/4 lg:-translate-y-1/4">
+      <div className="relative rounded-[32px] bg-yellow-light px-[15px] py-16 text-center lg:px-16 lg:py-[128px]">
+        <div className="absolute left-0 top-0 -translate-x-[86px] -translate-y-4 lg:-translate-x-1/4 lg:-translate-y-1/4">
           <img src={caroLayer} alt="caro layer" className="" />
         </div>
         <h3 className="title-heading !text-16 uppercase">
@@ -25,8 +25,8 @@ const Contact = () => {
         <h2 className="title-heading-2 lg:px-8">
           Prepare yourself & let’s explore the beauty of the world
         </h2>
-        <div className="mt-16">
-          <div className="flex flex-col items-center justify-between gap-x-16 gap-y-4 lg:flex-row">
+        <div className="mt-8 sm:mt-16">
+          <div className="flex flex-col items-center justify-between gap-x-4 gap-y-4 sm:flex-row md:gap-x-8 lg:gap-x-16">
             <div className="flex w-full flex-1 items-center gap-x-4 rounded-[32px] bg-white px-8 lg:w-auto">
               <div className="">
                 <img src={emailIcon} alt="email icon" />
@@ -39,7 +39,7 @@ const Contact = () => {
                 placeholder="Your Email"
               />
             </div>
-            <div className="w-full lg:w-auto">
+            <div className="w-full sm:w-auto">
               <Button
                 className="w-full !rounded-[32px] text-lg lg:px-[64px] lg:py-[34px] lg:text-[23px]"
                 onClick={handleSendEmail}
